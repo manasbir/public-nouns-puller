9bd596f93577b9b5ad103050399c1930ba34833a
manas <bagrimanasbir@gmail.com>
2023-06-13 13:53:31 -0700

    ok now done!

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::{fs, path::Path};
 
 fn main() {
-    if Path::new("/Users/manas/Documents/projectrs/public-nouns-puller/nouns").exists() == false {
-    fs::create_dir("/Users/manas/Documents/projectrs/public-nouns-puller/nouns").expect("failed to create dir");
+    if !Path::new("./nouns").exists() {
+    fs::create_dir("./nouns").expect("failed to create dir");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use base64::{Engine as _, engine::general_purpose};
 use ethers::{
-    core::types::{Address, Filter, H160},
-    providers::{Http, Middleware, Provider}, prelude::{abigen, rand::{seq::index, distributions::Standard}}, types::U256,
+    core::types::{Filter, H160},
+    providers::{Http, Middleware, Provider, StreamExt}, prelude::{abigen, types::U256},
 };
 use eyre::Result;
-use std::{sync::Arc, fs::{self, File}, path::Path, str::from_utf8, io::Write};
+use std::{sync::Arc, fs::File, str::from_utf8, io::Write};
 use dotenv::dotenv;
-use serde_json::{Result as json_result, Value};
+use serde_json::Value;
 
 abigen!(
     IERC721,
@@ -20,29 +20,41 @@ const NOUN_ADDRESS: &str = "0x93ecac71499147627DFEc6d0E494d50fCFFf10EE";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    dotenv().ok();
-    let provider = get_client().await;
-    let client = Arc::new(provider);
-    let noun_contract = IERC721::new(NOUN_ADDRESS.parse::<H160>()?, client);
-    let noun_uri = noun_contract.contract_uri().await?;
-
-    let current_nouns = check_dir();
     get_past_nouns().await;
-
+    listen_for_new_nouns().await;
 
 
     Ok(())
 }
 
-fn check_dir() -> i32 {
-    loop {
-        let mut i = 0;
-        let exists = Path::new(&format!("public-noun-{}", i)).exists();
-        if exists == false {
-            return i
+async fn listen_for_new_nouns() {
+    dotenv().ok();
+    let provider = get_client().await;
+    let client = Arc::new(provider);
+    let noun_contract = IERC721::new(NOUN_ADDRESS.parse::<H160>().expect("wrong address"), client.clone());
+    
+    let filter = Filter::new()
+        .address(noun_contract.address())
+        .event("NounCreated(uint256,(uint48,uint48,uint48,uint48,uint48))");
+
+    let mut stream = client.watch(&filter).await.unwrap();
+
+    while let Some(log) = stream.next().await {
+        println!("new noun created!");
+        let index = U256::from(log.topics[1].as_ref());
+        let noun_uri = noun_contract.token_uri(index).await;
+        match noun_uri {
+            Ok(res) => {
+                process_uri(&res, index);
+            }
+            Err(e) => {
+                println!("error: {:?}", e);
+                continue;
+            }
         }
-        i+=1;
     }
+    
+
 }
 
 async fn get_past_nouns() {
@@ -64,23 +76,7 @@ async fn get_past_nouns() {
         let noun_uri = noun_contract.token_uri(index).await;
         match noun_uri {
             Ok(res) => {
-                // println!("{:?}", &res[29..]);
-                let decoded = general_purpose::STANDARD.decode(&res[29..]).unwrap();
-                let json: Value = serde_json::from_str(from_utf8(&decoded).unwrap()).unwrap();
-                let image = general_purpose::STANDARD.decode(&json["image"].to_string()[27..&json["image"].to_string().len()-1]).unwrap();
-                let res = File::create(format!("nouns/public-noun-{}.svg", index)); 
-                match res {
-                    Ok(mut file) => {
-                        println!("file created");
-                        file.write_all(image.as_ref()).unwrap();
-                    }
-                    Err(e) => {
-                        println!("file already created");
-                        println!("error: {:?}", e);
-                        continue;
-                    }
-                }
-            
+                process_uri(&res, index);
             }
             Err(e) => {
                 println!("error: {:?}", e);
@@ -90,8 +86,21 @@ async fn get_past_nouns() {
     }
 }
 
-fn uri_to_svg() {
-
+fn process_uri(base64_string: &str, index: U256) {
+    let decoded = general_purpose::STANDARD.decode(&base64_string[29..]).unwrap();
+    let json: Value = serde_json::from_str(from_utf8(&decoded).unwrap()).unwrap();
+    let image = general_purpose::STANDARD.decode(&json["image"].to_string()[27..&json["image"].to_string().len()-1]).unwrap();
+    let res = File::create(format!("nouns/public-noun-{}.svg", index)); 
+    match res {
+        Ok(mut file) => {
+            // println!("file created");
+            file.write_all(image.as_ref()).unwrap();
+        }
+        Err(e) => {
+            println!("error: {:?}", e);
+            return;
+        }
+    }
 }
 
 async fn get_client() -> Provider<Http>  {
